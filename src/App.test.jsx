8cb6e5fdff67b1855ecf32582d78b440e8e78852
ari-harlem-caballero <ari.bash.harlem@gmail.com>
2022-04-25 15:52:08 -0700

    header test, added component

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,4 +1,12 @@
+import { render, screen} from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import Header from './components/layout/Header';
 
 test('Should render the header', () => {
+  render(
+    <MemoryRouter>
+    <Header />
+    </MemoryRouter>
+  );
 
 })
