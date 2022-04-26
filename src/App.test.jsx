5c689eeec540211b134e68484580b805043211dc
ari-harlem-caballero <ari.bash.harlem@gmail.com>
2022-04-25 21:21:52 -0700

    header test: profile name

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,11 +1,13 @@
 import { render, screen} from '@testing-library/react';
 import App from './App'
 
-test('Should render the header', () => {
+test('Should render the header', async () => {
   render(<App />)
     // header image
-  const image = screen.getAllByAltText('alchemy logo')
+  const image = screen.getByAltText('Alchemy Logo')
     // profile name
+  const headerName = await screen.findByLabelText(/meet vonta!/i)
 
   expect(image).toBeInTheDocument()
+  expect(headerName).toBeInTheDocument()
 })
